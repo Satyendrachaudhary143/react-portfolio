b1866119813bb5a526046d90e3ff2000dd7d6e43
satyendra chaudhary <148510277+Satyendrachaudhary143@users.noreply.github.com>
2024-11-23 12:40:17 +0530

    Update Home.jsx

--- src/Componant/Home/Home.jsx
+++ src/Componant/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "home.css"
+import "./home.css"
 import man from "../../assets/file/code.png";
 import TypingEffect from "react-typing-effect";
 import { MdOutlineWhatsapp } from 'react-icons/md';
