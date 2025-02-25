71de1f2f1f344c94778a53f9a8940d53f3e509fc
satyendra_chaudhary <satyendrachaudhary38@gmail.com>
2025-02-25 22:52:28 +0530

    new update

--- src/Componant/Home/Home.jsx
+++ src/Componant/Home/Home.jsx
@@ -1,4 +1,7 @@
 import "../../Css/style.css"
+import { FaLinkedin } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
 
 function Home() {
 
@@ -19,9 +22,9 @@ function Home() {
             </div>
             {/* <!-- Social Media Icons --> */}
             <div className="social-icons">
-               <a href="https://www.linkedin.com/in/satyendra-chaudhary-b958632b9?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" className="social-icon"><i className="fab fa-linkedin"></i></a>
-               <a href="https://github.com/satyendrachaudhary143" target="_blank" className="social-icon"><i className="fab fa-github"></i></a>
-               <a href="https://x.com/ssboss183032734" target="_blank" className="social-icon"><i className="fab fa-twitter"></i></a>
+               <a href="https://www.linkedin.com/in/satyendra-chaudhary-b958632b9?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" className="social-icon"><FaLinkedin/></a>
+               <a href="https://github.com/satyendrachaudhary143" target="_blank" className="social-icon"><FaGithub/></a>
+               <a href="https://x.com/ssboss183032734" target="_blank" className="social-icon"><FaTwitter/></a>
             </div>
          </div>
       </section>
--- src/Componant/Nav/Nav.jsx
+++ src/Componant/Nav/Nav.jsx
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import "../../Css/style.css"
 
 
 function Nav() {
 
- 
+ const [navbarOpen, setNavbarOpen] = useState(false);
 
 
 
@@ -13,10 +14,11 @@ function Nav() {
            <div className="container">
               {/* <!-- Logo --> */}
               <a className="navbar-brand" href="#">MyPortfolio</a>
-              {/* <!-- Hamburger Menu --> */}
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-              </button>
+             {/* <!-- Hamburger Menu --> */}
+           
+             
+              
+           
               {/* <!-- Navigation Links --> */}
               <div className="collapse navbar-collapse" id="navbarNav">
                  <ul className="navbar-nav ms-auto">
