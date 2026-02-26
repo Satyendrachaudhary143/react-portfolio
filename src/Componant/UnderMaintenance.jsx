5b124f8c3b0b7d2812a4e9538b1d1cb71802dd51
satyendra_chaudhary <satyendrachaudhary38@gmail.com>
2026-02-26 09:24:40 +0530

    redirect

--- src/Componant/UnderMaintenance.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export default function UnderMaintenance() {
-    useEffect(() => {
-        window.location.replace("https://satyendrachaudhary.in");
-    }, []);
-
-    return null;
-}
