013f7eaea955dc8859e5abc2277c5b7b9ea9cbaf
satyendra_chaudhary <satyendrachaudhary38@gmail.com>
2026-02-26 09:21:14 +0530

    new site is live

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import './App.css'
-import About from './Componant/About/About'
-import Contact from './Componant/Contact/Contact'
-import Home from './Componant/Home/Home'
-import Nav from './Componant/Nav/Nav'
-import Project from './Componant/Project/Project'
-import Tutorial from './Componant/Tutorial/Tutorial'
-import Skill from "./Componant/Skill/Skill"
-import Footer from "./Componant/Footer/Footer"
-import UnderMaintenance from "./Componant/UnderMaintenance"
+
 
 function App() {
-  return (
-    <>
-      <UnderMaintenance />
-      {/* <Nav />
-       <Home />
-      <About />
-      <Skill />
-      <Project />
-      <Tutorial />
-      <Contact />
-      <Footer/> */}
-    </>
-  )
+
+  useEffect(() => {
+    window.location.replace("https://satyendrachaudhary.in");
+  }, []);
+  return null
 }
 
 export default App
--- src/Componant/UnderMaintenance.jsx
+++ src/Componant/UnderMaintenance.jsx
@@ -1,39 +1,9 @@
-import React from "react";
-import "./UnderMaintenance.css";
+import { useEffect } from "react";
 
 export default function UnderMaintenance() {
-    return (
-        <div className="maintenance-wrapper">
-            <div className="maintenance-card">
-                <div className="status-badge">🚧 Currently Upgrading</div>
+    useEffect(() => {
+        window.location.replace("https://satyendrachaudhary.in");
+    }, []);
 
-                <h1>We’re Rebuilding Something Better.</h1>
-
-                <p className="description">
-                    This portfolio is temporarily offline while a new and improved
-                    version is being crafted. The next release will be faster, cleaner,
-                    and more powerful.
-                </p>
-
-                <div className="contact-box">
-                    <h3>Need immediate assistance?</h3>
-                    <p>Reach out directly:</p>
-
-                    <div className="contact-item">
-                        📞 <a href="tel:8840859444">8840859444</a>
-                    </div>
-
-                    <div className="contact-item">
-                        📧 <a href="mailto:satyendrachaudhary38@gmail.com">
-                            satyendrachaudhary38@gmail.com
-                        </a>
-                    </div>
-                </div>
-
-                <div className="footer-note">
-                    © {new Date().getFullYear()} Satyendra Chaudhary — All Rights Reserved
-                </div>
-            </div>
-        </div>
-    );
+    return null;
 }
